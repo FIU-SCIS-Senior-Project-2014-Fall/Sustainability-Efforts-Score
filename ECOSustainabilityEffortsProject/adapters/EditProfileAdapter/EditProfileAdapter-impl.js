e4983b3ea9a1ea7f92588c3646c75a1e9108862f
unknown <GeorgeM@SMS-GEORGEM.steiner.sll.com>
2014-10-11 20:57:10 -0400

    test 5

--- ECOSustainabilityEffortsProject/adapters/EditProfileAdapter/EditProfileAdapter-impl.js
+++
@@ -1,7 +0,0 @@
-
-function saveUserProfile(username, fname, lname, email, password) {
-	return WL.Server.invokeSQLStoredProcedure({
-		procedure : "save_user_profile",
-		parameters : [username, fname, lname, email, password]
-	});
-}
